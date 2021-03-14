bdfc11615440f8cc8e33efb56938f87a4074d3aa
kanna25280 <kannaiueo.wedupsy20@gmail.com>
2021-03-14 19:21:21 +0900

    1st pull request takami

---
+++ index.js
@@ -0,0 +1,19 @@
+new Chartist.Pie('.ct-chart', {
+  series: [20, 10, 30, 40]
+}, {
+  donut: true,
+  donutWidth: 60,
+  donutSolid: true,
+  startAngle: 270,
+  showLabel: true
+});
+
+new Chartist.Pie('.ct-chart', {
+  series: [20, 10, 30, 40]
+}, {
+  donut: true,
+  donutWidth: 60,
+  donutSolid: true,
+  startAngle: 270,
+  showLabel: true
+});
